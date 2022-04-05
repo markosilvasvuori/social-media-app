34689ac476a62a68666c93b8e3d2c32989b49b6d
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2022-04-05 15:56:52 +0300

    Added <HomeFeed/>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Layout/Header';
 import './App.css';
+import HomeFeed from './components/HomeFeed/HomeFeed';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <HomeFeed />
     </div>
   );
 }
