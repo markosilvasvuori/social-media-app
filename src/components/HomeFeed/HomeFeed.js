ddaceb57538357c30495c3774d223ced42e6e203
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2022-04-05 15:53:35 +0300

    Added useEffect to fetch posts

--- src/components/HomeFeed/HomeFeed.js
+++ src/components/HomeFeed/HomeFeed.js
@@ -1,53 +1,31 @@
+import { useEffect, useState } from 'react';
+import { getCollection } from '../../firebase/firebase';
+
+import Post from '../Post/Post';
 import classes from './HomeFeed.module.css';
 
-const DUMMY_POSTS = [
-    {
-        username: 'John Doe',
-        imageUrl: 'url',
-        caption: 'This is a test post and the first post at the same time',
-        likes: 10,
-        comments: [
-            {username: 'JaneDoe', comment: 'Nice photo!'},
-            {username: 'Tester', comment: 'Hello John'},
-        ]
-    },
-    {
-        username: 'Jane Doe',
-        imageUrl: 'url',
-        caption: 'This is a second post',
-        likes: 12,
-        comments: [
-            {username: 'JohnDoe', comment: 'Hi!'},
-            {username: 'Tester', comment: 'Hello'},
-        ]
-    }
-]
-
 const HomeFeed = () => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const getPosts = async () => {
+            const posts = await getCollection('posts');
+            setPosts(posts);
+        };
+
+        getPosts();
+    }, []);
+
     return (
         <div className={classes['home-feed']}>
-            {DUMMY_POSTS.map(post => (
-                <div>
-                    <header>
-                        <div>
-                            <img src='' alt='' />
-                            <p>{post.username}</p>
-                        </div>
-                        <button>...</button>
-                    </header>
-                    <div>
-                        <img src={post.imageUrl} alt='post' />
-                    </div>
-                    <div>
-                        <section>
-                            <button>Like</button>
-                            <button>Comment</button>
-                        </section>
-                        <section>{post.likes} likes</section>
-                        <section>{post.caption}</section>
-                        <a href='/'>Show all {post.comments.length} comments</a>
-                    </div>
-                </div>
+            {posts.map(post => (
+                <Post
+                    username={post.username}
+                    imageUrl={post.imageUrl}
+                    likes={post.likes}
+                    capiton={post.caption}
+                    comments={post.comments}
+                />
             ))}
         </div>
     );
