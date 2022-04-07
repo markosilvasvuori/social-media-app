1c9b77099a07636d53fff7f49f044fdff39c11db
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2022-04-07 19:54:50 +0300

    Added <MainNav />

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,23 +1,12 @@
 import Logo from '../UI/Logo';
+import MainNav from '../Navigation/MainNav';
 import classes from './Header.module.css';
 
 const Header = () => {
     return (
         <header className={classes.header}>
             <Logo size={'medium'} />
-            <nav className={classes.nav}>
-                <ul>
-                    <li>
-                        <a href='/'>Home</a>
-                    </li>
-                    <li>
-                        <a href='/'>Add</a>
-                    </li>
-                    <li>
-                        <a href='/'>Profile</a>
-                    </li>
-                </ul>
-            </nav>
+            <MainNav />
         </header>
     );
 };
