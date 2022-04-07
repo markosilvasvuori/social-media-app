eb8e637f2ad19e023dfe7bb93059397fc43355a1
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2022-04-07 19:30:06 +0300

    Started working with ProfileDetails

---
+++ src/components/Profile/ProfileDetails.js
@@ -0,0 +1,62 @@
+import { Fragment, useState, useContext } from 'react';
+
+import { AuthContext } from '../../store/auth-context';
+import Button from '../UI/Button';
+import classes from './ProfileDetails.module.css';
+
+const ProfileDetails = () => {
+    const {ctxValue} = useContext(AuthContext);
+    const [isEditing, setIsEditing] = useState(false);
+console.log(ctxValue.user);
+    const openSettingsHandler = () => {
+        setIsEditing(!isEditing);
+    };
+
+    return (
+        <Fragment>
+            {!isEditing &&
+                <header className={classes['user-header']}>
+                    <div className={classes['picture-container']}>
+                        <button>
+                            <img className={classes.picture} src='' alt='' />
+                        </button>
+                    </div>
+                    <div className={classes.details}>
+                        <div className={classes.top}>
+                            <h2>Username</h2>
+                            <Button onClick={openSettingsHandler}>Edit</Button>
+                        </div>
+                        <div className={classes.middle}>
+                            <ul>
+                                <li>
+                                    <span>10</span>
+                                    <span>Posts</span>
+                                </li>
+                                <li>
+                                    <span>42</span>
+                                    <span>Followers</span>
+                                </li>
+                                <li>
+                                    <span>25</span>
+                                    <span>Following</span>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className={classes.bottom}>
+                            <span className={classes.name}>Name</span>
+                            <span>Bio. Tell something about yourself.</span>
+                            <span><a href='#'>www.website.com</a></span>
+                        </div>
+                    </div>
+                </header>
+            }
+            {isEditing &&
+                <div>
+                    Settings
+                </div>
+            }
+        </Fragment>
+    );
+};
+
+export default ProfileDetails;
