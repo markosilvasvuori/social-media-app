9fdf7ab5c3a2891cb1781997f49ff83525d9edd7
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2022-04-08 13:30:29 +0300

    Added useEffect

--- src/components/Navigation/MainNav.js
+++ src/components/Navigation/MainNav.js
@@ -1,12 +1,18 @@
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import { UserContext } from '../../store/user-context';
 import classes from './MainNav.module.css';
 
 const MainNav = () => {
+    const [username, setUsername] = useState('');
     const {userCtx} = useContext(UserContext);
-    // const username = userCtx.user.username;
+
+    useEffect(() => {
+        if (userCtx.user) {
+            setUsername(userCtx.user.username);
+        }
+    }, [userCtx.user]);
 
     return (
         <nav className={classes.nav}>
@@ -18,7 +24,7 @@ const MainNav = () => {
                     <Link to='/'>Add</Link>
                 </li>
                 <li>
-                    <Link to={`profile/:${userCtx.user.username}`}>Profile</Link>
+                    <Link to={`profile/${username}`}>Profile</Link>
                 </li>
             </ul>
         </nav>
