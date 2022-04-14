f398b5a953c9b77763b2d138e38a6be8b5bf8856
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2022-04-14 20:01:59 +0300

    Rewrote post fetching

--- src/components/HomeFeed/HomeFeed.js
+++ src/components/HomeFeed/HomeFeed.js
@@ -1,29 +1,51 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import { getCollection } from '../../firebase/firebase';
+import { auth, firestoreDB } from '../../firebase/firebase';
+import { collection, doc, getDocs, onSnapshot } from 'firebase/firestore';
 
-import HomeFeedPost from '../Post/HomeFeedPost';
+import Post from '../Post/Post';
 import classes from './HomeFeed.module.css';
 
 const HomeFeed = () => {
     const [posts, setPosts] = useState([]);
+    const [followedUsers, setFollowedUsers] = useState([]);
+    const currentUserId = auth.currentUser.uid;
 
     useEffect(() => {
-        const getPosts = async () => {
-            const posts = await getCollection('posts');
-            setPosts(posts);
-        };
-
-        getPosts();
+        const userDataSnapshot = onSnapshot(doc(firestoreDB, 'users', currentUserId), (doc) => {
+            setFollowedUsers(doc.data().following);
+        });
     }, []);
 
     useEffect(() => {
-        console.log(posts);
-    }, [posts]);
+        const postsArray = [];
+        const getPosts = async () => {
+            const querySnapshot = await getDocs(collection(firestoreDB, 'users'));
+            querySnapshot.docs.map(async (doc, i) => {
+                const userQuery = followedUsers.length !== 0 ? followedUsers[i] : doc.id;
+                console.log('userQuery: ' + userQuery);
+                const posts = await getDocs(collection(firestoreDB, `users/${userQuery}/posts`));
+                posts.docs.map((post) => {
+                    postsArray.push(post.data());
+                });
+                setPosts(postsArray);
+            });
+        };
+
+        getPosts();
+
+        // const getPosts = async () => {
+        //     const posts = await getCollection('posts');
+        //     setPosts(posts);
+        // };
+
+        // getPosts();
+    }, [followedUsers]);
 
     return (
         <div className={classes['home-feed']}>
             {posts.map(post => (
-                <HomeFeedPost
+                <Post
                     key={post.postId}
                     userId={post.userId}
                     username={post.username}
