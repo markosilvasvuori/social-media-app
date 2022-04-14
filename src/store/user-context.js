285d4c32f2bd20a191f74effa3c7bebfaf749de7
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2022-04-14 21:27:54 +0300

    Added localStorage.getItem to restore user data if user is logged in

--- src/store/user-context.js
+++ src/store/user-context.js
@@ -7,14 +7,19 @@ export const UserContext = createContext('');
 export const UserProvider = (props) => {
     const [user, setUser] = useState(null);
 
+    if (!user) {
+        const storedUserData = localStorage.getItem('user');
+        setUser(storedUserData);
+    };
+
     const getUserData = async () => {
         const user = auth.currentUser;
         const docRef = doc(firestoreDB, 'users', user.uid);
         const docSnapshot = await getDoc(docRef);
 
         if (docSnapshot.exists()) {
-            // console.log(docSnapshot.data());
             setUser(docSnapshot.data());
+            localStorage.setItem('user', docSnapshot.data());
         } else {
             console.log('No such document!');
         }
