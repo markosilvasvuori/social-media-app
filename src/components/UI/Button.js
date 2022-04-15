51f18c730b32ee94058bd3781476c19674693591
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2022-04-15 21:44:49 +0300

    Removed unnecessary code from clickHandler

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -4,11 +4,7 @@ const Button = (props) => {
     const styles = `${classes.button} ${props.className ? props.className : ''}`;
 
     const clickHandler = () => {
-        if (props.onClick) {
-            props.onClick();
-        } else {
-            return;
-        }
+        props.onClick();
     }
 
     return (
