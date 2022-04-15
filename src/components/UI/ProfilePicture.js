06a55a49ee386b029e4ab20651093ef4bad386b7
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2022-04-15 21:46:48 +0300

    Added functionality to fetch profile picture

--- src/components/UI/ProfilePicture.js
+++ src/components/UI/ProfilePicture.js
@@ -1,8 +1,43 @@
+import { useState, useEffect, useContext } from 'react';
 import { Link } from 'react-router-dom';
 
+import { ref, getDownloadURL } from 'firebase/storage';
+import { storage } from '../../firebase/firebase';
+
+import { UserContext } from '../../store/user-context';
 import classes from './ProfilePicture.module.css';
 
 const ProfilePicture = ({ pictureUrl, size, userId }) => {
+    const [profilePictureUrl, setProfilePictureUrl] = useState(null);
+    const { userCtx } = useContext(UserContext);
+    const user = userCtx.user;
+
+    useEffect(() => {
+        const fetchProfilePicture = async () => {
+            if (!user.profilePicture) {
+                await getDownloadURL(ref(storage, 'assets/profile.png'))
+                .then((url) => {
+                    setProfilePictureUrl(url);
+                })
+                .catch((error) => {
+                    console.log(error.code);
+                    console.log(error.message);
+                });
+            } else {
+                await getDownloadURL(ref(storage, `users/${userId}/profilePicture/profile`))
+                .then((url) => {
+                    setProfilePictureUrl(url);
+                })
+                .catch((error) => {
+                    console.log(error.code);
+                    console.log(error.message);
+                });
+            };
+        };
+
+        fetchProfilePicture();
+    }, []);
+
     const styles = `${classes['profile-picture']} 
                     ${size === 'small' ? classes.small :
                     size === 'medium' ? classes.medium :
@@ -13,7 +48,7 @@ const ProfilePicture = ({ pictureUrl, size, userId }) => {
     return (
         <Link to={`profile/${userId}`}>
             <div className={styles}>
-                <img src={pictureUrl} alt='profile' />
+                <img src={profilePictureUrl} alt='profile' />
             </div>
         </Link>
     );
