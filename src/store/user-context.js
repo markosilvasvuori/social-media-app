b7e6ae62c869df40d9026f34bea9c02fe3c73b54
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2022-04-15 14:40:25 +0300

    Added follow and unfollow functions

--- src/store/user-context.js
+++ src/store/user-context.js
@@ -1,17 +1,19 @@
 import { createContext, useEffect, useState } from 'react';
 import { auth, firestoreDB } from '../firebase/firebase';
-import { doc, getDoc } from 'firebase/firestore';
+import { doc, getDoc, updateDoc, arrayUnion, arrayRemove } from 'firebase/firestore';
 
 export const UserContext = createContext('');
 
 export const UserProvider = (props) => {
     const [user, setUser] = useState(null);
 
+    // If user is already logged in, restore user data from localStorage
     if (!user) {
         const storedUserData = localStorage.getItem('user');
         setUser(storedUserData);
     };
 
+    // Get current user's data
     const getUserData = async () => {
         const user = auth.currentUser;
         const docRef = doc(firestoreDB, 'users', user.uid);
@@ -25,12 +27,36 @@ export const UserProvider = (props) => {
         }
     };
 
-    useEffect(() => {
-        console.log(user)
-    }, [user]);
+    const followHandler = async (userToFollowId) => {
+        const currentUserRef = doc(firestoreDB, 'users', user.userId);
+        const userToFollowRef = doc(firestoreDB, 'users', userToFollowId);
+
+        await updateDoc(currentUserRef, {
+            following: arrayUnion(userToFollowId)
+        });
+
+        await updateDoc(userToFollowRef, {
+            followers: arrayUnion(user.userId)
+        });
+    };
+
+    const unfollowHandler = async (userToUnfollowId) => {
+        const currentUserRef = doc(firestoreDB, 'users', user.userId);
+        const userToUnfollowRef = doc(firestoreDB, 'users', userToUnfollowId);
+
+        await updateDoc(currentUserRef, {
+            following: arrayRemove(userToUnfollowId)
+        });
+
+        await updateDoc(userToUnfollowRef, {
+            followers: arrayRemove(user.userId)
+        });
+    };
 
     const userCtx = {
         getUserData: getUserData,
+        follow: followHandler,
+        unfollow: unfollowHandler,
         user: user,
     };
 
