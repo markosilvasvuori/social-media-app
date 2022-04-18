88d3d034669f5443361300949b1c1d9800c725aa
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2022-04-18 14:50:12 +0300

    Imported styles

--- src/components/Post/CreatePostForm.js
+++ src/components/Post/CreatePostForm.js
@@ -1,13 +1,17 @@
-import { useContext, useState } from 'react';
+import { Fragment, useContext, useState } from 'react';
 
 import { PostContext } from '../../store/post-context';
+import { ModalContext } from '../../store/modal-context';
 import { auth } from '../../firebase/firebase';
 import Button from '../UI/Button';
+import classes from './CreatePostForm.module.css';
+import CloseButton from '../UI/CloseButton';
 
 const CreatePostForm = () => {
     const [file, setFile] = useState(null);
     const [caption, setCaption] = useState('');
-    const {postCtx} = useContext(PostContext);
+    const { postCtx } = useContext(PostContext);
+    const { modalCtx } = useContext(ModalContext);
     const userId = auth.currentUser.uid;
 
     const enteredFile = (event) => {
@@ -25,21 +29,30 @@ const CreatePostForm = () => {
     };
 
     return (
-        <form onSubmit={onSubmitHandler}>
-            <input 
-                type='file' 
-                name='file' 
-                accept='image/png, image/jpeg' 
-                onChange={enteredFile}
-            />
-            <textarea 
-                name='caption' 
-                placeholder='Write a caption...' 
-                value={caption}
-                onChange={enteredCaption}
-            />
-            <Button>Post</Button>
-        </form>
+        <Fragment>
+            <header className={classes.header}>
+                <h2>Make a post</h2>
+                <CloseButton onClick={modalCtx.modalHandler} />
+            </header>
+            <form
+                className={classes.form} 
+                onSubmit={onSubmitHandler}
+            >
+                <input 
+                    type='file' 
+                    name='file' 
+                    accept='image/png, image/jpeg' 
+                    onChange={enteredFile}
+                />
+                <textarea 
+                    name='caption' 
+                    placeholder='Write a caption...' 
+                    value={caption}
+                    onChange={enteredCaption}
+                />
+                <Button>Post</Button>
+            </form>
+        </Fragment>
     );
 };
 
