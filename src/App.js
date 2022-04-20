f921a3b6c4be07ebd65c00bcef20ba94dc531b20
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2022-04-20 12:29:30 +0300

    Added 404 route

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { Routes, Route } from 'react-router-dom';
 
 import { AuthContext } from './store/auth-context';
 import { UserContext } from './store/user-context';
+import { ModalContext } from './store/modal-context';
 import Header from './components/Layout/Header';
-import './App.css';
+import Modal from './components/Modal/Modal';
 
 import Home from './pages/Home';
 import Profile from './pages/Profile';
-import Modal from './components/Modal/Modal';
-import { ModalContext } from './store/modal-context';
+import Settings from './pages/Settings';
+import NotFound from './pages/NotFound';
+
 
 function App() {
   const {authCtx} = useContext(AuthContext);
@@ -33,12 +35,20 @@ function App() {
       }
       <Routes>
         <Route 
+          path='*'
+          element={<NotFound />} 
+        />
+        <Route 
           path='/' 
           element={<Home />} 
         />
         <Route 
           path='profile/:userId' 
           element={<Profile />} 
+        />
+        <Route 
+          path='profile/settings'
+          element={<Settings />}
         />
       </Routes>
     </Fragment>
