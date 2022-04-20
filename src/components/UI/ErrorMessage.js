7bb14a4f1b14f6003971c50363e7e8a5bad1c7d3
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2022-04-20 19:39:58 +0300

    Created ErrorMessage

---
+++ src/components/UI/ErrorMessage.js
@@ -0,0 +1,11 @@
+import classes from './ErrorMessage.module.css';
+
+const ErrorMessage = ({ message }) => {
+    return (
+        <div className={classes.error}>
+            <p>{message}</p>
+        </div>
+    );
+};
+
+export default ErrorMessage;
