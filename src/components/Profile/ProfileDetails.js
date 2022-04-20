edaf3b4a9ccbfbab0eda19aa923dce659dd63746
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2022-04-20 12:07:22 +0300

    Removed openSettingsHandler()

--- src/components/Profile/ProfileDetails.js
+++ src/components/Profile/ProfileDetails.js
@@ -1,5 +1,6 @@
 import { Fragment, useState, useEffect, useContext } from 'react';
 import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import { doc, getDoc } from 'firebase/firestore';
 import { firestoreDB } from '../../firebase/firebase';
@@ -12,7 +13,6 @@ import classes from './ProfileDetails.module.css';
 import UsersModalContent from '../Modal/Content/UsersModalContent';
 
 const ProfileDetails = () => {
-    const [isEditing, setIsEditing] = useState(false);
     const [userData, setUserData] = useState({});
     const [isFollowing, setIsFollowing] = useState(false);
     const { userId } = useParams();
@@ -40,10 +40,6 @@ const ProfileDetails = () => {
             setIsFollowing(true);
         }
     }, [userData]);
-
-    const openSettingsHandler = () => {
-        setIsEditing(!isEditing);
-    };
 
     const followHandler = async () => {
         await userCtx.follow(userId);
@@ -77,75 +73,68 @@ const ProfileDetails = () => {
     
     return (
         <Fragment>
-            {!isEditing &&
-                <header className={classes['user-header']}>
-                    <div className={classes['picture-container']}>
-                        <button>
-                            <ProfilePicture 
-                                userId={userId}
-                                size={'large'}
-                            />
-                        </button>
-                    </div>
-                    <div className={classes.details}>
-                        <div className={classes.top}>
-                            <h2>{userData.username}</h2>
-                            {userId === currentUser.userId &&
-                                <Button 
-                                    outline={true}
-                                    onClick={openSettingsHandler}>
+            <header className={classes['user-header']}>
+                <div className={classes['picture-container']}>
+                    <button>
+                        <ProfilePicture 
+                            userId={userId}
+                            size={'large'}
+                        />
+                    </button>
+                </div>
+                <div className={classes.details}>
+                    <div className={classes.top}>
+                        <h2>{userData.username}</h2>
+                        {userId === currentUser.userId &&
+                            <Link to='/profile/settings'>
+                                <Button outline={true}>
                                         Edit
                                 </Button>
-                            }
-                            {userId !== currentUser.userId && !isFollowing &&
-                                <Button 
-                                    onClick={followHandler}>
-                                        Follow
-                                </Button>
-                            }
-                            {userId !== currentUser.userId && isFollowing &&
-                                <Button 
-                                    outline={true}
-                                    onClick={unfollowHandler}>
-                                        Unfollow
-                                </Button>
-                            }
-                        </div>
-                        <div className={classes.middle}>
-                            <ul>
-                                <li>
-                                    <span>{userData.posts ? userData.posts.length : 0}</span>
-                                    <span>Posts</span>
-                                </li>
-                                <li 
-                                    className={classes.clickable} 
-                                    onClick={openFollowersHandler}
-                                >
-                                    <span>{userData.followers ? userData.followers.length : 0}</span>
-                                    <span>Followers</span>
-                                </li>
-                                <li 
-                                    className={classes.clickable} 
-                                    onClick={openFollowingHandler}
-                                >
-                                    <span>{userData.following ? userData.following.length : 0}</span>
-                                    <span>Following</span>
-                                </li>
-                            </ul>
-                        </div>
-                        <div className={classes.bottom}>
-                            <span className={classes.name}>{userData.name}</span>
-                            <span>{userData.bio}</span>
-                            <span><a href={userData.website}>{userData.website}</a></span>
-                        </div>
+                            </Link>
+                        }
+                        {userId !== currentUser.userId && !isFollowing &&
+                            <Button 
+                                onClick={followHandler}>
+                                    Follow
+                            </Button>
+                        }
+                        {userId !== currentUser.userId && isFollowing &&
+                            <Button 
+                                outline={true}
+                                onClick={unfollowHandler}>
+                                    Unfollow
+                            </Button>
+                        }
                     </div>
-                </header>
-            }
-            {isEditing &&
-                <div>
-                    Settings
+                    <div className={classes.middle}>
+                        <ul>
+                            <li>
+                                <span>{userData.posts ? userData.posts.length : 0}</span>
+                                <span>Posts</span>
+                            </li>
+                            <li 
+                                className={classes.clickable} 
+                                onClick={openFollowersHandler}
+                            >
+                                <span>{userData.followers ? userData.followers.length : 0}</span>
+                                <span>Followers</span>
+                            </li>
+                            <li 
+                                className={classes.clickable} 
+                                onClick={openFollowingHandler}
+                            >
+                                <span>{userData.following ? userData.following.length : 0}</span>
+                                <span>Following</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className={classes.bottom}>
+                        <span className={classes.name}>{userData.name}</span>
+                        <span>{userData.bio}</span>
+                        <span><a href={userData.website}>{userData.website}</a></span>
+                    </div>
                 </div>
-            }
+            </header>
         </Fragment>
     );
 };
