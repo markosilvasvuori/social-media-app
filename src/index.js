597ebe8069a37ae127a846421cb58522c433f065
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2022-04-27 20:33:12 +0300

    Added MenuModalProvider

--- src/index.js
+++ src/index.js
@@ -8,16 +8,19 @@ import { AuthProvider } from './store/auth-context';
 import { UserProvider } from './store/user-context';
 import { PostProvider } from './store/post-context';
 import { ModalProvider } from './store/modal-context';
+import { MenuModalProvider } from './store/menu-modal-context';
 
 ReactDOM.render(
   <React.StrictMode>
     <AuthProvider>
       <ModalProvider>
         <UserProvider>
-          <PostProvider>      
-            <BrowserRouter>
-              <App />
-            </BrowserRouter>
+          <PostProvider>  
+            <MenuModalProvider>    
+              <BrowserRouter>
+                <App />
+              </BrowserRouter>
+            </MenuModalProvider>
           </PostProvider>
         </UserProvider>
       </ModalProvider>
