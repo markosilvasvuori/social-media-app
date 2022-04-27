18dbbd7823b7c0f537d99cdf322e71c0cc6f8f48
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2022-04-27 20:29:51 +0300

    Added modal handlers

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,18 +1,27 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 
 import { ref, getDownloadURL } from 'firebase/storage';
-import { storage } from '../../firebase/firebase';
+import { auth, storage } from '../../firebase/firebase';
 import ProfilePicture from '../UI/ProfilePicture';
 
+import { ModalContext } from '../../store/modal-context';
+import { MenuModalContext } from '../../store/menu-modal-context';
+import MenuButton from '../UI/MenuButton';
+import PostModalContent from '../Modal/Content/PostModalContent';
 import classes from './Post.module.css';
+import MenuForOwnPosts from '../Modal/MenuModal/Content/MenuForOwnPosts';
+import MenuForPosts from '../Modal/MenuModal/Content/MenuForPosts';
 
-const Post = ({ userId, imageId, username, likes, caption, comments }) => {
+const Post = ({ userId, postId, username, likes, caption, comments, inModal = false }) => {
     const [imageUrl, setImageUrl] = useState(null);
+    const { modalCtx } = useContext(ModalContext);
+    const { menuModalCtx } = useContext(MenuModalContext);
+    const currentUser = auth.currentUser;
 
     useEffect(() => {
         const fetchImage = async () => {
-            await getDownloadURL(ref(storage, `posts/${imageId}`))
+            await getDownloadURL(ref(storage, `posts/${postId}`))
             .then((url) => {
                 setImageUrl(url);
             })
@@ -25,10 +34,54 @@ const Post = ({ userId, imageId, username, likes, caption, comments }) => {
         fetchImage();
     }, []);
 
+    const openInModalHandler = () => {
+        modalCtx.modalHandler(
+            <PostModalContent 
+                userId={userId}
+                postId={postId}
+                imageUrl={imageUrl}
+                username={username}
+                likes={likes}
+                caption={caption}
+                comments={comments}
+            />,
+            inModal ? false : true
+        );
+    };
+
+    const editPostHandler = () => {
+        modalCtx.modalHandler(
+            <PostModalContent 
+                userId={userId}
+                postId={postId}
+                imageUrl={imageUrl}
+                username={username}
+                likes={likes}
+                caption={caption}
+                editing={true} 
+            />,
+            inModal ? false : true
+        );
+    };
+
+    const showMenuHandler = () => {
+        menuModalCtx.menuHandler(
+            userId === currentUser.uid ?
+            <MenuForOwnPosts postId={postId} editPost={editPostHandler} /> :
+            <MenuForPosts userId={userId} />
+        );
+    };
+
+    const closeModalHandler = () => {
+        if (modalCtx.modal) {
+            modalCtx.modalHandler();
+        }
+    };
+
     return (
-        <div className={classes.post}>
+        <div className={`${classes.post} ${inModal ? classes['in-modal'] : ''}`}>
             <header className={classes.header}>
-                <div className={classes.user}>
+                <div className={classes.user} onClick={closeModalHandler}>
                     <ProfilePicture 
                         size={'small'} 
                         userId={userId}
@@ -37,11 +90,7 @@ const Post = ({ userId, imageId, username, likes, caption, comments }) => {
                         <p className={classes.bold}>{username}</p>
                     </Link>
                 </div>
-                <button className={classes['menu-button']}>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </button>
+                <MenuButton onClick={showMenuHandler} />
             </header>
             <div className={classes.content}>
                 <img src={imageUrl} alt='post' />
@@ -52,24 +101,28 @@ const Post = ({ userId, imageId, username, likes, caption, comments }) => {
                     <button>Comment</button>
                 </section>
                 <section>
-                    <div className={classes.bold}>
-                        {likes} likes
+                    <div className={classes.likes}>
+                        {likes.length} likes
                     </div>
                 </section>
                 <section>
-                    <p>
+                    <span>
                         <Link 
                             className={classes.bold} 
                             to={`profile/${userId}`}
+                            onClick={closeModalHandler}
                         >
                             {username}
                         </Link>
-                        {caption}
-                    </p>
+                        <span className={classes.caption}>{caption}</span>
+                    </span>
                 </section>
-                <a className={classes.comments} href='#'>
+                <button
+                    className={classes.comments} 
+                    onClick={openInModalHandler}
+                >
                     Show all {comments.length} comments
-                </a>
+                </button>
             </div>
         </div>
     );
