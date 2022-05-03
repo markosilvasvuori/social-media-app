e04da6545f8b68c8ef4db6bab43bf3cb997c069d
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2022-05-03 12:44:50 +0300

    Created PostPlaceholder

---
+++ src/components/Post/PostPlaceholder.js
@@ -0,0 +1,7 @@
+import classes from './PostPlaceholder.module.css';
+
+const PostPlaceholder = () => {
+    return <div className={classes.placeholder}></div>;
+};
+
+export default PostPlaceholder;
