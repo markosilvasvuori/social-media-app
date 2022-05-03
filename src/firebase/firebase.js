727ae2295eda7b5b0b75f0a4adec1a1ae02425a5
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2022-05-03 18:24:13 +0300

    Added required information

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -7,12 +7,12 @@ import { getStorage } from 'firebase/storage' ;
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
+  apiKey: "AIzaSyAUaH_pM6S0P8Xy2jnlj1-HNCXjCyFbZiw",
+  authDomain: "social-media-app-2bc8b.firebaseapp.com",
+  projectId: "social-media-app-2bc8b",
+  storageBucket: "social-media-app-2bc8b.appspot.com",
+  messagingSenderId: "1095065958990",
+  appId: "1:1095065958990:web:c959668af292cc3aed211f"
 };
 
 // Initialize Firebase
