aea7dd94b3ef55fb2de613ebfe4c710341da03f5
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2022-05-04 16:12:38 +0300

    Added Search and conditional rendering

--- src/components/Navigation/MainNav.js
+++ src/components/Navigation/MainNav.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { auth } from '../../firebase/firebase';
@@ -8,10 +8,16 @@ import addIcon from '../../images/addIcon.svg';
 import homeIcon from '../../images/homeIcon.svg';
 import classes from './MainNav.module.css';
 import ProfilePicture from '../UI/ProfilePicture';
+import Search from '../Search/Search';
 
 const MainNav = () => {
     const { modalCtx } = useContext(ModalContext);
     const currentUserId = auth.currentUser.uid;
+    const [isSearching, setIsSearching] = useState(false);
+
+    const onSearchHandler = () => {
+        setIsSearching(!isSearching);
+    };
 
     const addPostHandler = () => {
         modalCtx.modalHandler(<CreatePostForm />)
@@ -20,32 +26,41 @@ const MainNav = () => {
     return (
         <nav className={classes.nav}>
             <ul>
+                {!isSearching &&
+                    <li>
+                        <Link to='/'>
+                            <img 
+                                className={classes.icon} 
+                                src={homeIcon} 
+                                alt='home' 
+                            />
+                        </Link>
+                    </li>
+                }
                 <li>
-                    <Link to='/'>
-                        <img 
-                            className={classes.icon} 
-                            src={homeIcon} 
-                            alt='home' 
-                        />
-                    </Link>
+                    <Search onSearch={onSearchHandler} />
                 </li>
-                <li>
-                    <button onClick={addPostHandler}>
-                        <img 
-                            className={classes.icon} 
-                            src={addIcon} 
-                            alt='add' 
-                        />
-                    </button>
-                </li>
-                <li>
-                    <Link to={`profile/${currentUserId}`}>
-                        <ProfilePicture
-                            userId={currentUserId}
-                            size={'small'}
-                        />
-                    </Link>
-                </li>
+                {!isSearching &&
+                    <li>
+                        <button onClick={addPostHandler}>
+                            <img 
+                                className={classes.icon} 
+                                src={addIcon} 
+                                alt='add' 
+                            />
+                        </button>
+                    </li>
+                }
+                {!isSearching &&
+                    <li>
+                        <Link to={`profile/${currentUserId}`}>
+                            <ProfilePicture
+                                userId={currentUserId}
+                                size={'small'}
+                            />
+                        </Link>
+                    </li>
+                }
             </ul>
         </nav>
     );
