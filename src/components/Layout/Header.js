d1a971f902b4c90bee004763b75776aaf6a02b81
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2022-05-04 16:11:41 +0300

    Removed unused import

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,9 +1,9 @@
 import Logo from '../UI/Logo';
 import MainNav from '../Navigation/MainNav';
 import classes from './Header.module.css';
-import CreatePostForm from '../Post/CreatePostForm';
 
 const Header = () => {
+
     return (
         <header className={classes.header}>
             <Logo size={'medium'} />
